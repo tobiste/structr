d9b98c2a632ab8bb67fcde42244203da8ae6cb5a
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2023-08-10 19:13:14 -0400

    description of example data added

---
+++ R/example_data.R
@@ -0,0 +1,14 @@
+#' Example data from Gray et al. 1980 to find best small and great circle
+#'
+#' example data set
+#'
+#' @docType data
+#'
+#' @usage data('gray_example')
+#'
+#' @format An object of class `data.frame`
+#' @keywords datasets
+#' @examples
+#' data("gray_example")
+#' head("gray_example")
+"gray_example"
